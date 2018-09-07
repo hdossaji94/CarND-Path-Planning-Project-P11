2d0d4a1c56de466438172a56d52fb018d0ba09bd
Huzefa <hdossaji94@gmail.com>
2018-09-07 17:36:46 -0400

    fix redeclaration

--- src/main.cpp
+++ src/main.cpp
@@ -348,7 +348,7 @@ int main() {
             //In Frenet add evenly 30m spaced points ahead of the starting refernce
             vector<double> next_wp0 = getXY(car_s+30,(2+4*lane),map_waypoints_s,map_waypoints_x,map_waypoints_y);
             vector<double> next_wp1 = getXY(car_s+60,(2+4*lane),map_waypoints_s,map_waypoints_x,map_waypoints_y);
-            vector<double> next_wp0 = getXY(car_s+90,(2+4*lane),map_waypoints_s,map_waypoints_x,map_waypoints_y);
+            vector<double> next_wp2 = getXY(car_s+90,(2+4*lane),map_waypoints_s,map_waypoints_x,map_waypoints_y);
 
             ptsx.push_back(next_wp0[0]);
             ptsx.push_back(next_wp1[0]);
